c970d6353feddc5cbe22145d77ffe3bf09d42124
tchipi88 <ngansop.arthur@gmail.com>
2021-01-02 11:07:56 +0100

    Add Package Info

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -1,15 +1,52 @@
 //Splash Screen
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 import 'package:zentrip/utils/SizeConfig.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
+  Widget _infoTile(String title, String subtitle) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subtitle ?? 'Not set'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // You have to call it on your starting screen
     SizeConfig().init(context);
     return Scaffold(
-      body: Center(
-        child: Image.asset('assets/images/img1.jpg', width: 350.0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/images/img1.jpg', width: 350.0),
+          Text('Version ' + _packageInfo.version),
+        ],
       ),
     );
   }
